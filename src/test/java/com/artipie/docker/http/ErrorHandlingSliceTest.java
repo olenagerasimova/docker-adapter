b3e9bce04bead7d4ece88d5173fe6e31a95d0c7e
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-08-12 11:18:21 +0300

    #298 - Added test for normal request/response handling in ErrorHandlingSlice

--- src/test/java/com/artipie/docker/http/ErrorHandlingSliceTest.java
+++ src/test/java/com/artipie/docker/http/ErrorHandlingSliceTest.java
@@ -23,23 +23,85 @@
  */
 package com.artipie.docker.http;
 
+import com.artipie.asto.ext.PublisherAs;
 import com.artipie.docker.error.InvalidRepoNameException;
+import com.artipie.docker.proxy.AuthClientSlice;
 import com.artipie.http.Headers;
+import com.artipie.http.Response;
+import com.artipie.http.client.jetty.JettyClientSlices;
+import com.artipie.http.headers.Header;
+import com.artipie.http.hm.ResponseMatcher;
 import com.artipie.http.rq.RequestLine;
 import com.artipie.http.rq.RqMethod;
+import com.artipie.http.rs.RsFull;
 import com.artipie.http.rs.RsStatus;
+import com.artipie.http.rs.StandardRs;
 import io.reactivex.Flowable;
+import java.nio.ByteBuffer;
 import java.util.concurrent.CompletableFuture;
 import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 /**
  * Tests for {@link ErrorHandlingSlice}.
  *
  * @since 0.5
+ * @checkstyle ClassDataAbstractionCouplingCheck (500 lines)
  */
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
 class ErrorHandlingSliceTest {
+
+    @Test
+    void shouldPassRequestUnmodified() {
+        final String line = new RequestLine(RqMethod.GET, "/file.txt").toString();
+        final Header header = new Header("x-name", "some value");
+        final byte[] body = "text".getBytes();
+        new ErrorHandlingSlice(
+            (rqline, rqheaders, rqbody) -> {
+                MatcherAssert.assertThat(
+                    "Request line unmodified",
+                    rqline,
+                    new IsEqual<>(line)
+                );
+                MatcherAssert.assertThat(
+                    "Headers unmodified",
+                    rqheaders,
+                    Matchers.containsInAnyOrder(header)
+                );
+                MatcherAssert.assertThat(
+                    "Body unmodified",
+                    new PublisherAs(rqbody).bytes().toCompletableFuture().join(),
+                    new IsEqual<>(body)
+                );
+                return StandardRs.OK;
+            }
+        ).response(
+            line, new Headers.From(header), Flowable.just(ByteBuffer.wrap(body))
+        ).send(
+            (status, rsheaders, rsbody) -> CompletableFuture.allOf()
+        ).toCompletableFuture().join();
+    }
+
+    @Test
+    void shouldPassResponseUnmodified() {
+        final Header header = new Header("x-name", "some value");
+        final byte[] body = "text".getBytes();
+        final RsStatus status = RsStatus.OK;
+        final Response response = new AuthClientSlice(
+            new JettyClientSlices(),
+            (rsline, rsheaders, rsbody) -> new RsFull(
+                status,
+                new Headers.From(header),
+                Flowable.just(ByteBuffer.wrap(body))
+            )
+        ).response(new RequestLine(RqMethod.GET, "/").toString(), Headers.EMPTY, Flowable.empty());
+        MatcherAssert.assertThat(
+            response,
+            new ResponseMatcher(status, body, header)
+        );
+    }
 
     @Test
     void shouldHandleError() {
