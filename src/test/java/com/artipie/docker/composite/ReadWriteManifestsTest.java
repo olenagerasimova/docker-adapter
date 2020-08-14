7afdc584b0d9f4515b93720f05337bec184101fe
Alexandr <genryxy.alexandr@yandex.ru>
2020-08-14 17:02:56 +0300

    #268 - fix codestyle

--- src/test/java/com/artipie/docker/composite/ReadWriteManifestsTest.java
+++ src/test/java/com/artipie/docker/composite/ReadWriteManifestsTest.java
@@ -24,9 +24,6 @@
 package com.artipie.docker.composite;
 
 import com.artipie.asto.Content;
-import com.artipie.asto.Key;
-import com.artipie.asto.ext.PublisherAs;
-import com.artipie.docker.ExampleStorage;
 import com.artipie.docker.Manifests;
 import com.artipie.docker.manifest.Manifest;
 import com.artipie.docker.ref.ManifestRef;
@@ -35,7 +32,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.core.IsEqual;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -46,74 +43,89 @@ import org.junit.jupiter.params.provider.ValueSource;
  * @since 0.5
  */
 final class ReadWriteManifestsTest {
-    private static ManifestRef refcheck;
-    private static Content contentcheck;
-    private static Manifests mnfs;
+    /**
+     * Manifest reference.
+     */
+    private ManifestRef refcheck;
 
-    @BeforeAll
-    static void init() {
-        mnfs = new ReadWriteManifests(new ReadManifests(), new WriteManifests());
+    /**
+     * Manifest content.
+     */
+    private Content contentcheck;
+
+    /**
+     * Manifests.
+     */
+    private Manifests mnfs;
+
+    @BeforeEach
+    void init() {
+        this.mnfs = new ReadWriteManifests(new ReadManifests(), new WriteManifests());
     }
 
     @Test
     void shouldCallGetWithCorrectRef() {
         final ManifestRef ref = new ManifestRef.FromString("get");
-        mnfs.get(ref).toCompletableFuture().join();
+        this.mnfs.get(ref).toCompletableFuture().join();
         MatcherAssert.assertThat(
-            refcheck,
+            this.refcheck,
             new IsEqual<>(ref)
         );
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"data", "<p>**data**</p>"})
-    void shouldCallPutPassingCorrectData(String str) {
+    void shouldCallPutPassingCorrectData(final String str) {
         final byte[] data = str.getBytes();
         final ManifestRef ref = new ManifestRef.FromString(str);
-        mnfs.put(
+        this.mnfs.put(
             ref,
             new Content.From(data)
         ).toCompletableFuture().join();
         MatcherAssert.assertThat(
-            refcheck,
+            this.refcheck,
             new IsEqual<>(ref)
         );
         MatcherAssert.assertThat(
-            contentcheck.size().get(),
+            this.contentcheck.size().get(),
             new IsEqual<>((long) data.length)
         );
     }
 
-    private static class ReadManifests implements Manifests {
+    /**
+     * ReadManifests implementation.
+     *
+     * @since 0.5
+     */
+    private class ReadManifests implements Manifests {
         @Override
-        public CompletionStage<Manifest> put(ManifestRef ref, Content content) {
+        public CompletionStage<Manifest> put(final ManifestRef ref, final Content content) {
             throw new UnsupportedOperationException();
         }
 
         @Override
-        public CompletionStage<Optional<Manifest>> get(ManifestRef ref) {
-            refcheck = ref;
+        public CompletionStage<Optional<Manifest>> get(final ManifestRef ref) {
+            ReadWriteManifestsTest.this.refcheck = ref;
             return CompletableFuture.completedFuture(Optional.empty());
         }
     }
 
-    private static class WriteManifests implements Manifests {
+    /**
+     * WriteManifests implementation.
+     *
+     * @since 0.5
+     */
+    private class WriteManifests implements Manifests {
         @Override
         public CompletionStage<Manifest> put(final ManifestRef ref, final Content content) {
-            refcheck = ref;
-            contentcheck = content;
+            ReadWriteManifestsTest.this.refcheck = ref;
+            ReadWriteManifestsTest.this.contentcheck = content;
             return CompletableFuture.completedFuture(null);
         }
 
         @Override
-        public CompletionStage<Optional<Manifest>> get(ManifestRef ref) {
+        public CompletionStage<Optional<Manifest>> get(final ManifestRef ref) {
             throw new UnsupportedOperationException();
         }
-    }
-
-    private static byte[] bytes(final Key key) {
-        return new PublisherAs(
-            new ExampleStorage().value(key).join()
-        ).bytes().toCompletableFuture().join();
     }
 }
