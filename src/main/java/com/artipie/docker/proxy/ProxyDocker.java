79137eff5146306b68e8018c05d477a186ef6c66
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-12-16 11:50:50 +0300

    #376 - Catalog proxy

--- src/main/java/com/artipie/docker/proxy/ProxyDocker.java
+++ src/main/java/com/artipie/docker/proxy/ProxyDocker.java
@@ -23,11 +23,18 @@
  */
 package com.artipie.docker.proxy;
 
+import com.artipie.asto.Content;
+import com.artipie.asto.FailedCompletionStage;
+import com.artipie.asto.ext.PublisherAs;
 import com.artipie.docker.Catalog;
 import com.artipie.docker.Docker;
 import com.artipie.docker.Repo;
 import com.artipie.docker.RepoName;
+import com.artipie.http.Headers;
 import com.artipie.http.Slice;
+import com.artipie.http.rq.RequestLine;
+import com.artipie.http.rq.RqMethod;
+import com.artipie.http.rs.RsStatus;
 import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
@@ -35,9 +42,6 @@ import java.util.concurrent.CompletionStage;
  * Proxy {@link Docker} implementation.
  *
  * @since 0.3
- * @todo #354:30min Implement catalog method in ProxyDocker
- *  `catalog` method was added without proper implementation as placeholder.
- *  Method should be implemented and covered with unit tests.
  */
 public final class ProxyDocker implements Docker {
 
@@ -62,6 +66,25 @@ public final class ProxyDocker implements Docker {
 
     @Override
     public CompletionStage<Catalog> catalog(final Optional<RepoName> from, final int limit) {
-        throw new UnsupportedOperationException();
+        return new ResponseSink<>(
+            this.remote.response(
+                new RequestLine(RqMethod.GET, new CatalogUri(from, limit).string()).toString(),
+                Headers.EMPTY,
+                Content.EMPTY
+            ),
+            (status, headers, body) -> {
+                final CompletionStage<Catalog> result;
+                if (status == RsStatus.OK) {
+                    result = new PublisherAs(body).bytes().thenApply(
+                        bytes -> () -> new Content.From(bytes)
+                    );
+                } else {
+                    result = new FailedCompletionStage<>(
+                        new IllegalArgumentException(String.format("Unexpected status: %s", status))
+                    );
+                }
+                return result;
+            }
+        ).result();
     }
 }
