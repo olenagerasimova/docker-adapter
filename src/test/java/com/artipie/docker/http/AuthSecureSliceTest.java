b1dfa77f61e7a0410db1870ddc0ee8792d6b5889
Oleg Mozzhechkov <oleg.mozzhechkov@gmail.com>
2020-12-29 15:42:08 +0300

    #417 - Added SecureSlice and AuthSecureSlice

---
+++ src/test/java/com/artipie/docker/http/AuthSecureSliceTest.java
@@ -0,0 +1,103 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Artipie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package com.artipie.docker.http;
+
+import com.artipie.asto.Content;
+import com.artipie.http.Headers;
+import com.artipie.http.Response;
+import com.artipie.http.auth.AuthScheme;
+import com.artipie.http.auth.Authentication;
+import com.artipie.http.rs.StandardRs;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+
+/**
+ * Tests for {@link AuthSecureSlice}.
+ *
+ * @since 0.11
+ */
+class AuthSecureSliceTest {
+
+    @Test
+    void testScope() {
+        final String line = "GET /resource.txt HTTP/1.1";
+        final String action = "foo:bar:baz";
+        final AtomicReference<String> caction = new AtomicReference<>();
+        final AtomicReference<String> cline = new AtomicReference<>();
+        new AuthSecureSlice(
+            new SecureSlice() {
+                @Override
+                public Scope scope(final String rqline) {
+                    cline.set(rqline);
+                    return new Scope.FromString(action);
+                }
+
+                @Override
+                public Response response(
+                    final String line,
+                    final Iterable<Map.Entry<String, String>> headers,
+                    final Publisher<ByteBuffer> body
+                ) {
+                    return StandardRs.OK;
+                }
+            },
+            (AuthScheme) headers -> CompletableFuture.completedFuture(
+                new AuthScheme.Result() {
+                    @Override
+                    public Optional<Authentication.User> user() {
+                        return Optional.of(new Authentication.User("alice"));
+                    }
+
+                    @Override
+                    public String challenge() {
+                        return "";
+                    }
+                }
+            ),
+            (user, act) -> {
+                caction.set(act);
+                return true;
+            }
+        ).response(line, Headers.EMPTY, Content.EMPTY).send(
+            (status, headers, body) -> CompletableFuture.allOf()
+        ).toCompletableFuture().join();
+        MatcherAssert.assertThat(
+            "Request line passed to secure slice",
+            cline.get(),
+            new IsEqual<>(line)
+        );
+        MatcherAssert.assertThat(
+            "Scope passed as action to permissions",
+            caction.get(),
+            new IsEqual<>(action)
+        );
+    }
+}
